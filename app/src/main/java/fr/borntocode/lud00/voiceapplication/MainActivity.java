4b5d18d8580140e998c1bbdd4d1b0bc76ed407e7
lvimont <ludovic.vimont@gmail.com>
2016-04-18 22:34:37 +0200

    Correct EXTRA_LANGUAGE issue thanks to nbenm and correct main_layour for all devices.

--- app/src/main/java/fr/borntocode/lud00/voiceapplication/MainActivity.java
+++ app/src/main/java/fr/borntocode/lud00/voiceapplication/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     private void promptSpeechInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault().getDisplayLanguage());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Vous pouvez parler ...");
 
         try {
